737ccbc7e1089f0b242beb3bc92a845236610b87
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2023-06-12 00:54:17 -0300

    Correção de bug do tamanho dos frames

--- boardtivity/src/main/java/br/com/boardtivity/service/Frame.java
+++ boardtivity/src/main/java/br/com/boardtivity/service/Frame.java
@@ -14,23 +14,27 @@ public class Frame {
         //Post envio = new Post(token, idBoard);
        
         try {
-        	List<StrateegiaData> listaDeComentarios = StrateegiaData.loadDataFromFile("C:\\Users\\andre\\Desktop\\"+ arquivo +"");
-
+        	//List<StrateegiaData> listaDeComentarios = StrateegiaData.loadDataFromFile("C:\\Users\\andre\\Desktop\\"+ arquivo +"");
+        	///boardtivity/src/main/java/br/com/boardtivity/data/data.txt
+        	String caminhoDoArquivo = System.getProperty("user.dir") + "\\src\\main\\java\\br\\com\\boardtivity\\data\\" + arquivo;
+        	List<StrateegiaData> listaDeComentarios = StrateegiaData.loadDataFromFile(caminhoDoArquivo);
         	
         	int i = 0;
             int j = 0;
             int k = 0;
+            
+            if(numeroInstancias == 1) {
+                posicao = 0;
+            }
+            else {
+            	posicao = posicao + 255;
+            }
+            
             int posY = posicao;
+            
             int posX = 0;
             int posYFrame = 0;
             int posXFrame = 0;
-            
-            if(numeroInstancias == 1) {
-            	posYFrame = 0 ;
-                posXFrame = 0;
-            }
-            
-            
             int largura = 300;
             int altura = 200;
             
@@ -38,7 +42,7 @@ public class Frame {
            
             	
             String questao = listaDeComentarios.get(0).getQuestao();
-            envio.gerarStickerNote(questao, "red", "-255", Integer.toString(posicao));
+            envio.gerarStickerNote(questao, "red", "-255", Integer.toString(posY));
             
          
             int maior = listaDeComentarios.get(0).getRepostas().size();
@@ -48,7 +52,7 @@ public class Frame {
             	if(questao != listaDeComentarios.get(j).getQuestao()) {
             		posX = posX + 255;
             		largura = largura + 255;
-            		envio.gerarStickerNote(listaDeComentarios.get(j).getQuestao(), "red", Integer.toString(posX), Integer.toString(posicao));
+            		envio.gerarStickerNote(listaDeComentarios.get(j).getQuestao(), "red", Integer.toString(posX), Integer.toString(posY));
             		questao = listaDeComentarios.get(j).getQuestao();
             		posX = posX + 255;
             		largura = largura + 255;
@@ -89,19 +93,21 @@ public class Frame {
             
             altura = altura + ((maior + 1) * 255);
             
+            posXFrame = (largura - 500 - 300)/2;
+            
             if(numeroInstancias == 1) {
-            	posXFrame = (largura - 500 - 300)/2;
                 posYFrame = (altura - 255 - 200)/2;
             }
             else {
-            	posXFrame = (largura - 500 - 300)/2;
-            	posYFrame = posicao + 250;
+            	posYFrame = posicao - 255 + altura/2;
             }
-            System.out.println(numeroInstancias);
             
             String retorno = envio.gerarBackground(tituloFrame, Integer.toString(posXFrame), Integer.toString(posYFrame), Integer.toString(altura), Integer.toString(largura));
             System.out.println(retorno);
-            return altura;
+             
+            return posYFrame + altura/2;
+            
+            
             
         } catch (IOException e) {
         	e.printStackTrace();
--- boardtivity/src/main/java/br/com/boardtivity/service/ProgramaFrame.java
+++ boardtivity/src/main/java/br/com/boardtivity/service/ProgramaFrame.java
@@ -2,14 +2,13 @@ package br.com.boardtivity.service;
 
 public class ProgramaFrame {
 	public static void main(String[] args){
-		String token = "eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_JiBFHqHds3jax-Rgh0AuQRpc41I";
+		String token = "eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_pF3IkxnJO-aaBUqxlZTLLyGcTAM";
         String idBoard = "uXjVOzzQtpM=";
 
         Post envio = Post.getInstance(token, idBoard);
 		
         int posicao = Frame.gerarFrame("Fale sobre você", envio, "data.txt", 0);
-        int posicao2 = Frame.gerarFrame("Ambientes", envio, "data2.txt", posicao + 250);
-		
-        //int posicao3 = Frame.gerarFrame("Perguntas aleatórias", envio, "data3.txt", (posicao2*2) + (250*2));
+        int posicao2 = Frame.gerarFrame("Ambientes", envio, "data2.txt", posicao + 300);
+        int posicao3 = Frame.gerarFrame("Perguntas aleatórias", envio, "data3.txt", posicao2 + 300);
 	}
 }
